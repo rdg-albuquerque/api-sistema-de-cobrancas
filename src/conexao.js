52ccd651870310b29f4bd532e4a360b15fe55729
Rodrigo Albuquerque <rodrigomedeiros4996@gmail.com>
2021-11-29 04:43:16 -0300

    scripts, porta e ssl configuradas para o heroku

--- src/conexao.js
+++ src/conexao.js
@@ -1,17 +1,20 @@
-const {Pool} = require('pg');
+const { Pool } = require("pg");
 
-const pool = new Pool ({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'desafio_final',
-    password: '123456',
-    port: 5432
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "desafio_final",
+  password: "123456",
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 const query = (text, param) => {
-    return pool.query(text, param);
-}
+  return pool.query(text, param);
+};
 
 module.exports = {
-    query
-}
+  query,
+};
