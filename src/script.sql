5e6a3a726af1ef04cacbc02dfeef0603e6d0d0d8
Rodrigo Albuquerque <rodrigomedeiros4996@gmail.com>
2021-11-30 21:29:29 -0300

    Update script.sql

--- src/script.sql
+++ src/script.sql
@@ -11,7 +11,7 @@ create table if not exists usuarios (
 
 create table if not exists clientes (
 	id serial primary key,
-  	usuario_id integer not null references usuarios (id),
+	inadimplencia boolean default false
   	nome text not null,
   	email text not null unique,
   	telefone text not null,
