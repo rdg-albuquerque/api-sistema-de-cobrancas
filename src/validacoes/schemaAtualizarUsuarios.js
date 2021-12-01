c90fb3eea430f4f10bcaa4b50870b5a9faa33225
Rodrigo Albuquerque <rodrigomedeiros4996@gmail.com>
2021-12-01 00:54:58 -0300

    fix: ajuste validação de senha atualizar usuário
    Agora só pede o mínimo caso a senha seja maior que 0, ou seja, não for uma string vazia.

--- src/validacoes/schemaAtualizarUsuarios.js
+++ src/validacoes/schemaAtualizarUsuarios.js
@@ -1,11 +1,14 @@
-const yup = require('./configuracao');
+const yup = require("./configuracao");
 
 const schemaAtualizarUsuario = yup.object().shape({
-    nome: yup.string().required(),
-    email: yup.string().required().email(),
-    senha: yup.string().min(5),
-    cpf: yup.string(),
-    telefone: yup.string()
-})
+  nome: yup.string().required(),
+  email: yup.string().required().email(),
+  senha: yup.string().when({
+    is: (senha) => senha.length > 0,
+    then: yup.string().min(5),
+  }),
+  cpf: yup.string(),
+  telefone: yup.string(),
+});
 
 module.exports = schemaAtualizarUsuario;
